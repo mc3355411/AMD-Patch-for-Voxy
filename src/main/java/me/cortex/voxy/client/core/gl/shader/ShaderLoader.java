b8c341f51d86aa7c185853fb58069c7e16eda275
mc3355411 <87313470+mc3355411@users.noreply.github.com>
2026-02-14 02:21:11 +0800

    Create ShaderLoader.java

---
+++ src/main/java/me/cortex/voxy/client/core/gl/shader/ShaderLoader.java
@@ -0,0 +1,11 @@
+package me.cortex.voxy.client.core.gl.shader;
+
+/**
+ * 存根类，仅在编译时使用，运行时将被 Voxy 的实际类替代。
+ */
+public class ShaderLoader {
+    public static String parse(String id) {
+        // 此方法不会被调用，仅用于让编译器通过
+        return "";
+    }
+}
